c8d93755ba53b7438ee81a6d02bf883090186031
urushibata <urush1bata.lala@gmail.com>
2022-09-21 17:51:34 +0900

    add delete old backup file script.

---
+++ backup/delete_old_backup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. config
+
+facility=${SYSLOG_FACILITY:-syslog}
+
+logger -p ${facility}.notice -t $0 "[$$] minecraft backup delete start."
+
+delete_files=$(rclone lsl ${RCLONE_DEST_PATH} | awk '{ if(dueDate > $2" "$3) {print dir"/"$4}}' dir=${RCLONE_DEST_PATH} dueDate="$(date "+%F %T.000000000" "--date" "-${SAVE_DAYS} days")")
+
+for f in $delete_files;do
+  rclone delete $f 2>&1 | logger -p ${facility}.notice -t $0
+  end_code=${PIPESTATUS[0]}
+
+  if [[ ${end_code} != "0" ]]; then
+    logger -p ${facility}.notice -t $0 "[$$] minecraft backup delete failed.endcode: ${end_code}"
+    exit ${end_code}
+  fi
+done
+
+logger -p ${facility}.notice -t $0 "[$$] minecraft backup delete success."
